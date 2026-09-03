d2674bc552d9f94637ca874306ea267b4ec556f3
tuthanika <tuthanika@gmail.com>
2026-09-03 15:13:23 +0700

    v3.64mod

--- drivers/dropbox/meta.go
+++ drivers/dropbox/meta.go
@@ -17,8 +17,11 @@ type Addition struct {
 	ClientID      string `json:"client_id" required:"false" help:"Keep it empty if you don't have one"`
 	ClientSecret  string `json:"client_secret" required:"false" help:"Keep it empty if you don't have one"`
 
-	AccessToken     string
-	RootNamespaceId string
+	AccessToken     string `json:"AccessToken" required:"false"`
+	RootNamespaceId string `json:"RootNamespaceId" required:"false"`
+	
+	UseOnlineAPI    bool   `json:"use_online_api" default:"false"`
+	APIAddress      string `json:"api_url_address" default:"https://api.oplist.org/dropboxs/renewapi"`
 }
 
 var config = driver.Config{
